da358fc84ef4eed5a41ee7ba4c859255eee69033
Raushan Kumar <raushangnit@gmail.com>
2023-08-10 19:07:15 +0530

    Catalog API with MongoDB and Docker Implementation

---
+++ src/Services/Catelog/Catelog.API/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catelog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement]
+        public string  Name { get; set; }
+
+        [BsonElement]
+        public string Summary { get; set; }
+
+        [BsonElement]
+        public string Category { get; set; }
+
+        [BsonElement]
+        public string Description { get; set; }
+
+        [BsonElement]
+        public string ImageFile { get; set; }
+
+        [BsonElement]
+        public decimal Price { get; set; }
+
+    }
+}
