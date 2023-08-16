73777254fe54f592eb83c3559e488331fd77a383
Raushan kumar <raushangnit@gmail.com>
2023-08-16 11:46:14 +0530

    Basket API and Catalog API changes with Docker Configuration

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -31,7 +31,7 @@ namespace Catelog.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catelog.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
             });
 
             services.AddScoped<ICatalogContext, CatalogContext>();
