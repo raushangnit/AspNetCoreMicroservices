848c54e339e748336dab900b84da456c013cab0f
Raushan kumar <raushangnit@gmail.com>
2023-08-18 21:09:23 +0530

    Ordering API with Docker Configuration

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/IUnhandledExceptionBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    public interface IUnhandledExceptionBehaviour<TRequest, TResponse>
+    {
+        Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() { UserName = "raushangnit", FirstName = "Raushan", LastName = "Kumar", EmailAddress = "raushangnit@gmail.com", AddressLine = "E101,Goldnwinds,Lohegaon,Pune", Country = "India", TotalPrice = 350 }
+            };
+        }
+    }
+}
